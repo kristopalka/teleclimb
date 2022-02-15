c3af1f85bd11df17bd585e7545d2d026586c6b89
kristopalka <kristopalka@gmail.com>
2022-02-15 12:56:29 +0100

    test data update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO CATEGORY (NAME, SHORT_NAME, FROM_AGE, TO_AGE)
+INSERT INTO CATEGORY (ID, NAME, SHORT_NAME, FROM_AGE, TO_AGE)
 VALUES
-    ('Dziecko młodsze', 'DM', 0, 9),
-    ('Dziecko', 'D', 10, 11),
-    ('Młodzik', 'Mł', 12, 13),
-    ('Junior młodszy', 'JM', 14, 15),
-    ('Junior', 'J', 16, 17),
-    ('Młodzieżowiec', 'M', 18, 19),
-    ('Senior', 'S', 20, 1000);
+    (1, 'Dziecko młodsze', 'DM', 0, 9),
+    (2, 'Dziecko', 'D', 10, 11),
+    (3, 'Młodzik', 'Mł', 12, 13),
+    (4, 'Junior młodszy', 'JM', 14, 15),
+    (5, 'Junior', 'J', 16, 17),
+    (6, 'Młodzieżowiec', 'M', 18, 19),
+    (7, 'Senior', 'S', 20, 1000);
 
-INSERT INTO FORMULA (NAME, IDENTIFIER, DISCIPLINE, DESCRIPTION)
+INSERT INTO FORMULA (ID, NAME, DISCIPLINE, DESCRIPTION)
 VALUES
-    ( 'Prowadzenie', 'DEFAULT', 'BOULDERING', 'Dwie rundy: eliminacyjna i finałowa. Dwie drogi eliminacyjne i jedna finałowa' );
+    (1, 'Standardowe prowadzenie', 'LEAD', 'Dwie rundy: eliminacyjna i finałowa. Dwie drogi eliminacyjne i jedna finałowa. ' );
