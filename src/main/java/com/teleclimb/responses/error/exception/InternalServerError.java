aea2e917c6921ede9fd7928d0b4a22fa5650b27f
kristopalka <kristopalka@gmail.com>
2022-02-15 11:42:07 +0100

    addes internal server error

---
+++ src/main/java/com/teleclimb/responses/error/exception/InternalServerError.java
@@ -0,0 +1,9 @@
+package com.teleclimb.responses.error.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InternalServerError extends TeleclimbException {
+    public InternalServerError(String message) {
+        super(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
