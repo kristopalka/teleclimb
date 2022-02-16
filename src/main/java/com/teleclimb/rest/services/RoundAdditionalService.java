d2687d906a826165972475ba9eb3fd5198db9297
kristopalka <kristopalka@gmail.com>
2022-02-16 09:37:25 +0100

    remove

--- src/main/java/com/teleclimb/rest/services/RoundAdditionalService.java
+++
@@ -1,9 +0,0 @@
-package com.teleclimb.rest.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class RoundAdditionalService {
-    //todo dodawanie dróg do tury
-    //todo generowanie startów
-}
