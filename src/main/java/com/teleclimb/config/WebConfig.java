dd2c459522840350f77a81ee60b43a5a407596d8
kristopalka <kristopalka@gmail.com>
2022-04-25 13:40:57 +0200

    webconf update

--- src/main/java/com/teleclimb/config/WebConfig.java
+++ src/main/java/com/teleclimb/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedOriginPatterns("http://localhost:3000");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOriginPatterns("");
     }
 }
